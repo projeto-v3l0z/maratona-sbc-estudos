5fb05a2b2d9d8c4381fab73e8f4f3f5ba1ea2b77
johnpgr <jp.greidinger@hotmail.com>
2026-06-07 16:58:22 -0300

    feat(joao): add problema A in kotlin and c++

---
+++ joao/A.kt
@@ -0,0 +1,70 @@
+package joao
+
+/*
+ * Enunciado: 
+ * M turmas e N tipos de frutas diferentes
+ *
+ * m: Turma -> para cada fruta x
+ *                        ^-- alunos que gostam.
+ *                        e.g. Fruta {liked_by: ['aluno1', 'aluno2', 'aluno3']}
+ *
+ * Porem nao temos dados individuais de alunos.
+ *    e.g. Aluno { likes: ['fruta1', 'fruta2', 'fruta3'] }
+ *
+ * E tambem nao sabemos quantos alunos ha em cada turma.
+ * 
+ * Problema:
+ * Com base nos dados das frutas(quantos alunos gostam de cada fruta EM CADA TURMA)
+ * Determinar o menor numero POSSIVEL de alunos que a escola pode ter.
+ * Sabendo que:
+ *  - cada turma tem pelo menos 1 aluno;
+ *  - cada aluno pertence a unica turma;
+ *  - cada aluno gosta de PELO MENOS 1 fruta;
+ *  - um mesmo aluno pode gostar de varias frutas;
+ *
+ * Escola{turmas: Turma[]}
+ * Turma{alunos: Aluno[min:1]}
+ * Aluno{gosta: Fruta[min:1]}
+ * Fruta{gostado_por: Aluno[]}
+ *
+ * Input:
+ * Primeira linha:
+ * <integer> <integer> => N e M (N >= 1, M <= 1000) => numero de frutas(N) numero de turmas(M)
+ * Cada uma das {N} linhas seguintes(frutas) contem {M} (numero de alunos) que gostam naquela turma.
+ * formando uma tabela G[i][j]
+ *                               Turma 1   Turma 2   Turma 3
+ *                       Fruta 1      20        15        14
+ *                       Fruta 2      12        20        12
+ *                       Fruta 3      18         5        10
+ * Estrategia:
+ * Encontrar o maior numero em cada coluna(turma);
+ * Este numero sera o numero minimo de alunos nesta turma.
+ * Somar cada um desses numeros minimos de cada turma 
+ * e entao encontramos o numero minimo de alunos na escola.
+ *
+ */
+
+fun stdinLines(): Sequence<String> = System.`in`.bufferedReader().lineSequence()
+
+object A {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val lines = stdinLines().iterator()
+        val firstLine = lines.next()
+        val (n, m) = firstLine.split(" ").map { it.toInt() }
+        val maxByTurma = IntArray(m)
+
+        repeat(n) {
+            val values = lines.next().split(" ").map { it.toInt() }
+
+            values.forEachIndexed { i, v -> 
+                if (v > maxByTurma[i]) {
+                    maxByTurma[i] = v 
+                    // println("UPDATE: maxByTurma[$i] = ${maxByTurma[i]}")
+                }
+            }
+        }
+        println(maxByTurma.sum())
+    }
+}
+
